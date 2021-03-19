e3f06c7656fe62da376bb50dcb9603c135d37495
mralimov <mralimov89@gmail.com>
2021-03-19 17:51:50 -0400

    Calculate money insterted

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ def order_coffee(order):
             return False
         else:
             return True
+
+
+def calc_money_inserted(money_inserted, cost):
+    """Returns True if there coins inserted enough to buy"""
+    if money_inserted >= cost:
+        change = round(money_inserted - cost, 2)
+        print(f"Here is your ${change} in change")
+        global balance
+        balance += cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
